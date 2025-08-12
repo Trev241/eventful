5d27329ae4f3ebcd14f571f6629efdb223bd50de
Trev241 <trevisliu@gmail.com>
2025-08-12 16:16:30 -0400

    Initialize spring project

---
+++ src/main/java/edu/humber/demo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package edu.humber.demo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EventfulApplication.class);
+	}
+
+}
---
+++ src/test/java/edu/humber/demo/EventfulApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.humber.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
